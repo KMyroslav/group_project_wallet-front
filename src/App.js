62f7ab3c607043491b722b874e0918f48d2110eb
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-12 17:46:11 +0200

    add base and second fonts

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 import s from './App.module.scss';
 
+import { ReactComponent as IconWallet } from '../src/icons/IconWallet.svg';
+
 function App() {
   return (
     <div className={s.container}>
-      <header>Header</header>
+      <header>
+        <h1 className={s.logo}>
+          <IconWallet />
+          Wallet
+        </h1>
+      </header>
       <main>main</main>
       <footer>footer</footer>
     </div>
