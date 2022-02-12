eed01be385dc98e9d544819d20a32d3c61637cb2
ruslan <kuzrus31@gmail.com>
2022-02-12 19:55:47 +0300

    использовал библиотеки formic и yup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import RegistrationForm from './components/RegistrationForm/RegistrationForm'
 import s from './App.module.scss';
+import { Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className={s.container}>
+
+      <Route path='/' component={RegistrationForm} />
       <header>Header</header>
       <main>main</main>
       <footer>footer</footer>
---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,110 @@
+import React from "react";
+import './RegistrationForm.scss';
+import { Formik, Form } from "formik";
+import * as yup from "yup";
+import { NavLink } from "react-router-dom";
+
+
+export default function RegistrationForm() {
+
+
+    const validationsSchema = yup.object().shape({
+        name: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+
+        password: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+        confirmPassword: yup.string().oneOf([yup.ref('password')], 'Пароли не совпадают').required('Обязательно'),
+        email: yup.string().email('Введите верный email').required('Обязательно'),
+
+    })
+
+
+    return (
+
+        <>
+            <Formik
+                initialValues={{
+                    email: "",
+                    password: "",
+                    confirmPassword: "",
+                    name: "",
+                }}
+                validateOnBlur
+                onSubmit={values => { console.log(values) }}
+                validationSchema={validationsSchema}
+            >
+                {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+
+                    <Form className="form-register ">
+                        <div>
+
+                            <h1>Wallet</h1>
+
+
+                            <label htmlFor={`email`} className="label" >
+
+                                <input type="email"
+                                    email="email"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder="E-mail"
+                                    value={values.email}
+                                    id="email" />
+
+                            </label>
+
+                            {touched.email && errors.email && <p className="errors">{errors.email}</p>}
+
+                            <label htmlFor={`password`} className="label">
+
+                                <input
+                                    type="password"
+                                    name="password"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    placeholder=" Пароль"
+                                    value={values.password}
+                                />
+                            </label>
+
+                            {touched.password && errors.password && <p className="errors">{errors.password}</p>}
+
+                            <label htmlFor={`confirmPassword`} className="label">
+
+                                <input
+                                    type="confirmPassword"
+                                    name="confirmPassword"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.confirmPassword}
+                                    placeholder=" Подтвердите пароль"
+                                />
+                            </label>
+
+                            {touched.confirmPassword && errors.confirmPassword && <p className="errors">{errors.confirmPassword}</p>}
+
+                            <label htmlFor={`name`} className="label">
+
+                                <input
+                                    type="name"
+                                    name="name"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.name}
+                                    placeholder="Ваше имя"
+                                />
+                            </label>
+                            {touched.name && errors.name && <p className="errors">{errors.name}</p>}
+                            <button onClick={handleSubmit} disabled={!isValid && !dirty} type="submit">Регистрация</button>
+
+                            <div className="btn-container">
+                                <NavLink to="/login" className="main_btn">
+                                    Вход
+                                </NavLink>
+                            </div>
+                        </div>
+                    </Form>)
+                }
+            </Formik >
+        </>
+    );
+}
