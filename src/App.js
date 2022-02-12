fcd8b86963885bd1ef742692469e35b050b506ba
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-12 16:36:32 +0200

    add formik and yup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import s from './App.module.css';
+import s from './App.module.scss';
 
 function App() {
-  return <div className={s.container}>Hello team dev!!!</div>;
+  return (
+    <div className={s.container}>
+      <header>Header</header>
+      <main>main</main>
+      <footer>footer</footer>
+    </div>
+  );
 }
 
 export default App;
