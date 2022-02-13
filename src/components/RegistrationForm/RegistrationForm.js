7da7ec858ec0de355986b2712d27396cc24c8243
ruslan <kuzrus31@gmail.com>
2022-02-13 23:06:19 +0300

    доделал библиотеку yup.Стилизировал  css

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,18 +1,20 @@
 import React from "react";
-import './RegistrationForm.scss';
+
 import { Formik, Form } from "formik";
 import * as yup from "yup";
 import { NavLink } from "react-router-dom";
 import ButtonRegister from "../ButtonRegister/ButtonRegister.js"
-
+import './RegistrationForm.scss';
+import s from '../Header/Header.module.scss'
+import { ReactComponent as IconWallet } from '../../icons/IconWallet.svg';
 
 export default function RegistrationForm() {
 
 
     const validationsSchema = yup.object().shape({
-        name: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+        name: yup.string().min(1, "Имя должно состоять минимум из 1 символа").max(12, "Имя должно состоять максимум из 12 символов").typeError('Должно быть строкой').required('Обязательно'),
 
-        password: yup.string().typeError('Должно быть строкой').required('Обязательно'),
+        password: yup.string().min(6, "Пароль должен состоять минимум из 6 символов").max(14, "Пароль должен состоять максимум из 14 символов").typeError('Должно быть строкой').required('Обязательно'),
         confirmPassword: yup.string().oneOf([yup.ref('password')], 'Пароли не совпадают').required('Обязательно'),
         email: yup.string().email('Введите верный email').required('Обязательно'),
 
@@ -36,79 +38,82 @@ export default function RegistrationForm() {
                 {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
 
                     <Form className="form-register ">
-                        <div className="logo_reg_wrapper">
 
 
+                        <div className="logo-container" >
+                            <IconWallet className={s.logoIcon} />
+                            <h1 className={s.logo}>Wallet</h1>
+                        </div>
 
 
-                            <label htmlFor={`email`} className="label" >
+                        <label htmlFor={`email`} className="label" >
 
-                                <input type="email"
+                            <input type="email"
 
-                                    email="email"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    placeholder="E-mail"
-                                    value={values.email}
-                                    id="email"
-                                    className="input" />
+                                email="email"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                placeholder="E-mail"
+                                value={values.email}
+                                id="email"
+                                className="input" />
 
-                            </label>
+                        </label>
 
-                            {touched.email && errors.email && <p className="errors">{errors.email}</p>}
+                        {touched.email && errors.email && <p className="errors">{errors.email}</p>}
 
-                            <label htmlFor={`password`} className="label">
+                        <label htmlFor={`password`} className="label">
 
-                                <input
-                                    type="password"
-                                    name="password"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    placeholder=" Пароль"
-                                    value={values.password}
-                                    className="input"
-                                />
-                            </label>
+                            <input
+                                type="password"
+                                name="password"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                placeholder=" Пароль"
+                                value={values.password}
+                                className="input"
+                            />
+                        </label>
 
-                            {touched.password && errors.password && <p className="errors">{errors.password}</p>}
+                        {touched.password && errors.password && <p className="errors">{errors.password}</p>}
 
-                            <label htmlFor={`confirmPassword`} className="label">
+                        <label htmlFor={`confirmPassword`} className="label">
 
-                                <input
-                                    type="confirmPassword"
-                                    name="confirmPassword"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.confirmPassword}
-                                    placeholder=" Подтвердите пароль"
-                                    className="input"
-                                />
-                            </label>
+                            <input
+                                type="confirmPassword"
+                                name="confirmPassword"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.confirmPassword}
+                                placeholder=" Подтвердите пароль"
+                                className="input"
+                            />
+                        </label>
 
-                            {touched.confirmPassword && errors.confirmPassword && <p className="errors">{errors.confirmPassword}</p>}
+                        {touched.confirmPassword && errors.confirmPassword && <p className="errors">{errors.confirmPassword}</p>}
 
-                            <label htmlFor={`name`} className="label">
+                        <label htmlFor={`name`} className="label">
 
-                                <input
-                                    type="name"
-                                    name="name"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    value={values.name}
-                                    placeholder="Ваше имя"
-                                    className="input"
-                                />
-                            </label>
-                            {touched.name && errors.name && <p className="errors">{errors.name}</p>}
-
+                            <input
+                                type="name"
+                                name="name"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                                placeholder="Ваше имя"
+                                className="input"
+                            />
+                        </label>
+                        {touched.name && errors.name && <p className="errors">{errors.name}</p>}
+                        <div className="btn-container">
                             <ButtonRegister className="logo_btn" onClick={handleSubmit} disabled={!isValid && !dirty} disable="sd" type="submit" text="Регистрация" />
 
-                            <div className="btn-container">
-                                <NavLink to="/login" className="main_btn">
-                                    Вход
-                                </NavLink>
-                            </div>
+
+                            <NavLink to="/login" className="main_btn">
+                                Вход
+                            </NavLink>
                         </div>
+
                     </Form>)
                 }
             </Formik >
