36bbc8128b2e4eae60354836ae2df40312cb7841
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-14 19:04:37 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import s from './App.module.scss';
 
-import RegistrationPage from './pages/RegistrationPage/RegistrationPage.js';
+import RegistrationPage from 'pages/RegistrationPage';
 import Header from 'pages/Header';
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
     <div className={s.container}>
       <Header />
       <Switch>
-        <Route path="/" component={RegistrationPage} />
+        <Route path="/" component={<RegistrationPage />} />
       </Switch>
       <main>main</main>
       <footer>footer</footer>
