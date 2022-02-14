54844f7fb0a6137fc9b02b4e440934cf184aa5aa
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-14 18:58:19 +0200

    header-fix

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import s from './Header.module.scss';
 
+import { ReactComponent as IconWallet } from '../../icons/IconWallet.svg';
 import { ReactComponent as IconExit } from '../../icons/exit.svg';
 
 export default function Header() {
   return (
     <>
       <header className={s.container}>
-
-
+        <div>
+          <IconWallet className={s.logoIcon} />
+          <h1 className={s.logo}>Wallet</h1>
+        </div>
         <div className={s.userStatus}>
           <p>Имя</p>
           <IconExit className={s.exitIcon} />
