3aea6a73d8df13df3778543d3ed17137363a4731
KMyroslav <kukhtarukm@gmail.com>
2022-02-17 21:54:07 +0200

    Creates dash board page

---
+++ src/views/DashBoardPage/DashBoardPage.js
@@ -0,0 +1,41 @@
+import Media from 'react-media';
+
+import styles from './DashBoardPage.module.scss';
+
+import Navigation from 'components/Navigation';
+import Balance from 'components/Balance';
+import TableHome from 'components/TableHome';
+import Currency from 'components/Currency';
+import Header from 'components/Header';
+import { Route } from 'react-router-dom';
+
+export default function DashBoardPage() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.blur}>
+        <div className={styles.container}>
+          <Header />
+          <div className={styles.navTableWrap}>
+            <div className={styles.navBalanceCurrencyWrap}>
+              <div className={styles.navBalanceWrap}>
+                <div className={styles.navigation}>
+                  <Navigation />
+                </div>
+                <Route path="/home/currency">
+                  <Currency />
+                </Route>
+                <div className={styles.balance}>
+                  <Balance />
+                </div>
+              </div>
+              <Media query="(min-width: 768px)">
+                <Currency />
+              </Media>
+            </div>
+            <TableHome />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
