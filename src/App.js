190eed2bbfef52d0387a5419ac7b5d92ea8a72c3
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-17 22:07:43 +0200

    fix deploy

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import './App.scss';
 import RegistrationPage from './views/RegistrationPage/RegistrationPage';
 import LoginPage from './views/LoginPage/LoginPage';
 
+import Header from 'components/Header';
+
 function App() {
   return (
     <div className="container">
+      <Header />
       <Switch>
         <Route exact path="/" component={RegistrationPage} />
         <Route path="/login" component={LoginPage} />
