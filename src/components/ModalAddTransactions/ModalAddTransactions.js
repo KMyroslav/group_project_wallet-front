2b35e898698bc366dc9d5bc766437773f87b7c76
Alexander Tkachuk <tkachuka2020@gmail.com>
2022-02-20 01:10:55 +0200

    Markup, styles and custom components for ModalAddTransaction added.

--- src/components/ModalAddTransactions/ModalAddTransactions.js
+++ src/components/ModalAddTransactions/ModalAddTransactions.js
@@ -1,52 +1,123 @@
-import { useFormik } from 'formik';
-// import * as Yup from 'yup';
+import { useState } from 'react';
 import styles from './ModalAddTransactions.module.scss';
+import 'styles/globalMUI.scss';
 import { ReactComponent as CloseModalIcon } from 'icons/CloseModalIcon.svg';
+import { ReactComponent as DateRangeIcon } from 'icons/DateRangeIcon.svg';
+import { ReactComponent as OpenSelectIcon } from 'icons/OpenSelectIcon.svg';
+
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import ruLocale from 'date-fns/locale/ru';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import TextField from '@mui/material/TextField';
 
 import Toggler from 'components/Toggler';
+import CustomNumberFormat from 'components/CustomNumberFormat';
 
 const ModalAddTransactions = () => {
-  const formik = useFormik({
-    initialValues: {
-      type: 'expenses',
-      amount: 0.0,
-      timestamp: '',
-      comment: '',
-    },
-  });
+  const [toggled, setToggled] = useState('expenses');
+  const [timestamp, setTimestamp] = useState(new Date());
+  const [amount, setAmount] = useState(null);
+
+  // const [selected, setSelected] = useState(null);
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+
+  const locale = ruLocale;
+  const mask = '__.__.___';
+
+  const handleToggle = () => {
+    toggled === 'expenses' ? setToggled('profit') : setToggled('expenses');
+  };
+
+  const onAmountChange = (event) => {
+    const { value } = event.target;
+    setAmount(value);
+  };
 
   return (
-    <div>
+    <div className={styles.formWrapper}>
       <button className={styles.closeModalButton}>
         <CloseModalIcon />
       </button>
       <p className={styles.formCall}>Добавить транзакцию</p>
-      <Toggler />
-      <form>
-        <label>
-          Доход
+      <form className={styles.form}>
+        <Toggler selected={toggled} toggleSelected={handleToggle} />
+        <div className={styles.selectWrapper}>
           <input
-            id="type"
-            type="radio"
-            name="type"
-            onChange={formik.handleChange}
+            className={
+              toggled === 'expenses'
+                ? styles.transactionCategory
+                : styles.hidden
+            }
+            placeholder="Выберите категорию"
           />
-        </label>
-        <label>
-          Расход
-          <input
-            id="type"
-            type="radio"
-            name="type"
-            onChange={formik.handleChange}
+          <button
+            className={
+              toggled === 'expenses' ? styles.openSelectButton : styles.hidden
+            }
+            type="button"
+            onClick={() => setIsSelectOpen(!isSelectOpen)}
+          >
+            <OpenSelectIcon />
+          </button>
+          {isSelectOpen && (
+            <div className={styles.selectListWrapper}>
+              <ul className={styles.selectList}>{}</ul>
+            </div>
+          )}
+        </div>
+        <div className={styles.neighborInputsWrapper}>
+          <TextField
+            variant="outlined"
+            label=""
+            placeholder="0.00"
+            value={amount === null ? '' : amount}
+            onChange={onAmountChange}
+            name="numberformat"
+            id="formatted-numberformat-input"
+            InputProps={{
+              inputComponent: CustomNumberFormat,
+              classes: { notchedOutline: styles.noBorder },
+            }}
           />
-        </label>
-        <select></select>
-        <input id="amount" type="number" name="amount" />
-        <input id="timestamp" type="date" name="timestamp" />
-        <textarea id="comment" name="comment"></textarea>
-        <button type="submit">Добавить</button>
-        <button type="button">Отмена</button>
+          <LocalizationProvider dateAdapter={DateAdapter} locale={locale}>
+            <DatePicker
+              components={{ OpenPickerIcon: DateRangeIcon }}
+              desktopModeMediaQuery="@media (min-width: 320px)"
+              classes={{ notchedOutline: styles.noBorder }}
+              mask={mask}
+              minDate={new Date()}
+              value={timestamp}
+              onChange={(date) => {
+                setTimestamp(date);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  helperText={null}
+                  sx={{
+                    '& .MuiInputBase-input': {
+                      color: '#000000;',
+                      fontWeight: 'regular',
+                    },
+                  }}
+                />
+              )}
+            />
+          </LocalizationProvider>
+        </div>
+        <textarea
+          className={styles.transactionComment}
+          id="comment"
+          name="comment"
+          placeholder="Комментарий"
+        ></textarea>
+        <button className={styles.addTransactionBtn} type="submit">
+          Добавить
+        </button>
+        <button className={styles.cancelTransactionBtn} type="button">
+          Отмена
+        </button>
       </form>
     </div>
   );
--- src/components/Toggler/Toggler.js
+++ src/components/Toggler/Toggler.js
@@ -1,9 +1,43 @@
 import styles from './Toggler.module.scss';
+import { ReactComponent as AddTransactionsIcon } from 'icons/AddTransactionsIcon.svg';
+import { ReactComponent as ExpensesTransactionIcon } from 'icons/ExpensesTransactionsIcon.svg';
 
 const Toggler = ({ selected, toggleSelected }) => {
+  const isTransactionPositive = selected === 'profit';
+  const isTransactionNegative = selected === 'expenses';
+
   return (
-    <div className={styles.container} onClick={toggleSelected}>
-      <button className={styles.button}></button>
+    <div
+      className={
+        isTransactionPositive ? styles.withoutMargins : styles.togglerWrapper
+      }
+    >
+      <p
+        className={
+          isTransactionPositive ? styles.activeProfit : styles.plainProfit
+        }
+      >
+        Доход
+      </p>
+      <div className={styles.container} onClick={toggleSelected}>
+        <button
+          type="button"
+          className={isTransactionPositive ? styles.profit : styles.expenses}
+        >
+          {isTransactionPositive ? (
+            <AddTransactionsIcon />
+          ) : (
+            <ExpensesTransactionIcon />
+          )}
+        </button>
+      </div>
+      <p
+        className={
+          isTransactionNegative ? styles.activeExpenses : styles.plainExpenses
+        }
+      >
+        Расход
+      </p>
     </div>
   );
 };
