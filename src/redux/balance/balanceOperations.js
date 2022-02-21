374e67a56434afdba2961b6f727e4a29b808f48e
KMyroslav <kukhtarukm@gmail.com>
2022-02-21 15:06:20 +0200

    Removes unwanted instance

--- src/redux/balance/balanceOperations.js
+++ src/redux/balance/balanceOperations.js
@@ -1,24 +1,11 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const BASE_URL = 'https://dvf-project-group-2-back.herokuapp.com/';
-
-axios.defaults.baseURL = BASE_URL;
-
-const retrievedStoreStr = localStorage.getItem('persist:token');
-const retrievedStore = JSON.parse(retrievedStoreStr);
-const token = retrievedStore.token.slice(1, retrievedStore.token.length - 1);
-
-const instance = axios.create({
-  baseURL: BASE_URL,
-  headers: { Authorization: 'Bearer ' + token },
-});
-
 export const fetchBalance = createAsyncThunk(
   'finance/fetchBalance',
   async (_, { rejectWithValue }) => {
     try {
-      const balance = await instance.get('/api/users/current');
+      const balance = await axios.get('/api/users/current');
       return balance.data;
     } catch (error) {
       return rejectWithValue(error.message);
