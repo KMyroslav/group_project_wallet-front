8d67e2729239564c852f18e070e7daa1bd7acfa7
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-21 12:35:17 +0200

    edited slyle registranion pages

--- src/views/RegistrationPage/RegistrationPage.js
+++ src/views/RegistrationPage/RegistrationPage.js
@@ -5,7 +5,8 @@ import { useWindowSize } from '@react-hook/window-size';
 
 import RegistrationForm from '../../components/RegistrationForm';
 
-import LoginLogo from '../../images/Frame-tblet-reg.png';
+// import LoginLogo from '../../images/Frame-tblet-reg.png';
+import LoginLogo2x from '../../images/Frame-desctop@2x.png';
 
 import styles from './RegistrationPage.module.scss';
 
@@ -13,26 +14,24 @@ const RegistrationPage = () => {
   const [width] = useWindowSize();
 
   return (
-    <div className={styles.pageWrapper}>
-      <div className="container">
-        <div className={styles.mainWrapper}>
-          <Media query="(min-width: 768px)">
-            <div className={styles.logoWrapper}>
-              {width >= 768 && (
-                <img
-                  src={LoginLogo}
-                  alt="finance app logo"
-                  className={styles.logo}
-                />
-              )}
-              {width >= 768 && <p className={styles.text}>Finance App</p>}
-            </div>
-          </Media>
+    <div className="container">
+      <div className={styles.mainWrapper}>
+        <Media query="(min-width: 768px)">
+          <div className={styles.logoWrapper}>
+            {width >= 768 && (
+              <img
+                src={LoginLogo2x}
+                alt="finance app logo"
+                className={styles.logo}
+              />
+            )}
+            {width >= 768 && <p className={styles.text}>Finance App</p>}
+          </div>
+        </Media>
 
-          <div className={styles.blur}>
-            <div className={styles.formWrapper}>
-              <RegistrationForm />
-            </div>
+        <div className={styles.blur}>
+          <div className={styles.formWrapper}>
+            <RegistrationForm />
           </div>
         </div>
       </div>
