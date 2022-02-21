83188ce1e118a2de9b2a198d892b301210841eea
KMyroslav <kukhtarukm@gmail.com>
2022-02-21 19:43:51 +0200

    fixes categories

--- src/components/TableHome/TableHome.js
+++ src/components/TableHome/TableHome.js
@@ -5,15 +5,17 @@ import DesktopTable from './DesktopTable';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTransactions } from 'redux/transactionsTable/transactionsTableOperations';
 import getTransactionsTable from 'redux/transactionsTable/transactionsTableSelectors';
-import selectors from '../../redux/statistics/statisticsSelectors';
+import { fetchCategories } from 'redux/categories/categoriesOperations';
+import categoriesActions from 'redux/categories/categoriesSelectors';
 import { useEffect } from 'react';
 
 export default function TableHome() {
   const dispatch = useDispatch();
   const data = useSelector(getTransactionsTable);
-  const categories = useSelector(selectors.getCategoriesList);
+  const categories = useSelector(categoriesActions.getCategories);
 
   useEffect(() => {
+    dispatch(fetchCategories());
     dispatch(fetchTransactions());
   }, [dispatch]);
 
@@ -21,9 +23,9 @@ export default function TableHome() {
     <Media query="(max-width: 768px)">
       {(matches) =>
         matches ? (
-          <MobileTable data={data} categories={categories.data} />
+          <MobileTable data={data} categories={categories} />
         ) : (
-          <DesktopTable data={data} categories={categories.data} />
+          <DesktopTable data={data} categories={categories} />
         )
       }
     </Media>
