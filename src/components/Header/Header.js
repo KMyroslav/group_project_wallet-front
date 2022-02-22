1e00f0c8674a24e79db88dc13c7d69a2e9597dea
Denys Filichkin <denys.philichkin@gmail.com>
2022-02-22 10:55:18 +0200

    fix light

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+
 import { Link, NavLink } from 'react-router-dom';
 import { useWindowSize } from '@react-hook/window-size';
 
