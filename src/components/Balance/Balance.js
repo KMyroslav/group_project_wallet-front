98ae2ce8fa0b718929a97a2bbcb45ad7a136cc79
KMyroslav <kukhtarukm@gmail.com>
2022-02-23 18:56:12 +0200

    Updates balance refresh

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -1,9 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchBalance } from 'redux/balance/balanceOperations';
 import getBalance from 'redux/balance/balanceSelectors';
+import getNewTransaction from 'redux/transaction/transactionSelectors';
 import styles from './Balance.module.scss';
 
 export default function Balance() {
+  const dispatch = useDispatch();
   const balance = useSelector(getBalance);
+  const newTransaction = useSelector(getNewTransaction);
+
+  useEffect(() => {
+    dispatch(fetchBalance());
+  }, [dispatch, newTransaction]);
+
   return (
     <div className={styles.wrapper}>
       <p className={styles.title}>Ваш баланс</p>
