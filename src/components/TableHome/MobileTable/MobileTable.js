d0cb588c85fda9340ccc744c765cea8bfc707a23
KMyroslav <kukhtarukm@gmail.com>
2022-02-24 00:20:29 +0200

    Creates mobile data plug

--- src/components/TableHome/MobileTable/MobileTable.js
+++ src/components/TableHome/MobileTable/MobileTable.js
@@ -1,5 +1,5 @@
 import { isAfter, format } from 'date-fns/esm';
-import style from './MobileTable.module.scss';
+import styles from './MobileTable.module.scss';
 import { useSelector } from 'react-redux';
 import getTransactionsTable from 'redux/transactionsTable/transactionsTableSelectors';
 import categoriesActions from 'redux/categories/categoriesSelectors';
@@ -10,58 +10,72 @@ export default function MobileTable() {
   const sortedData = [...data].sort((a, b) =>
     isAfter(new Date(a.date), new Date(b.date)) ? -1 : 1,
   );
-  return sortedData.map(
-    ({ _id, date, typeTx, categoryId, comment, sum, balance }) => {
-      const category = categories.find((el) => el._id === categoryId);
-      return (
-        <div
-          className={
-            typeTx === 'income'
-              ? `${style.tableWrapper} ${style.wrapperIncome}`
-              : `${style.tableWrapper}`
-          }
-          key={_id}
-        >
-          <ul className={style.list}>
-            <li className={style.listItem}>
-              <span className={style.listKey}>Дата</span>
-              <span className={style.listValue}>
-                {format(new Date(date), 'dd.MM.yy')}
-              </span>
-            </li>
-            <li className={style.listItem}>
-              <span className={style.listKey}>Тип</span>
-              <span className={style.listValue}>
-                {typeTx === 'income' ? '+' : '-'}
-              </span>
-            </li>
-            <li className={style.listItem}>
-              <span className={style.listKey}>Категория</span>
-              <span className={style.listValue}>{category?.nameCategory}</span>
-            </li>
-            <li className={style.listItem}>
-              <span className={style.listKey}>Комментарий</span>
-              <span className={style.listValue}>{comment}</span>
-            </li>
-            <li className={style.listItem}>
-              <span className={style.listKey}>Сумма</span>
-              <span
-                className={
-                  typeTx === 'income'
-                    ? `${style.listValue} ${style.income}`
-                    : `${style.listValue} ${style.outcome}`
-                }
-              >
-                {sum.toFixed(2)}
-              </span>
-            </li>
-            <li className={style.listItem}>
-              <span className={style.listKey}>Баланс</span>
-              <span className={style.listValue}>{balance.toFixed(2)}</span>
-            </li>
-          </ul>
+  return (
+    <>
+      {sortedData.map(
+        ({ _id, date, typeTx, categoryId, comment, sum, balance }) => {
+          const category = categories.find((el) => el._id === categoryId);
+          return (
+            <div
+              className={
+                typeTx === 'income'
+                  ? `${styles.tableWrapper} ${styles.wrapperIncome}`
+                  : `${styles.tableWrapper}`
+              }
+              key={_id}
+            >
+              <ul className={styles.list}>
+                <li className={styles.listItem}>
+                  <span className={styles.listKey}>Дата</span>
+                  <span className={styles.listValue}>
+                    {format(new Date(date), 'dd.MM.yy')}
+                  </span>
+                </li>
+                <li className={styles.listItem}>
+                  <span className={styles.listKey}>Тип</span>
+                  <span className={styles.listValue}>
+                    {typeTx === 'income' ? '+' : '-'}
+                  </span>
+                </li>
+                <li className={styles.listItem}>
+                  <span className={styles.listKey}>Категория</span>
+                  <span className={styles.listValue}>
+                    {category?.nameCategory}
+                  </span>
+                </li>
+                <li className={styles.listItem}>
+                  <span className={styles.listKey}>Комментарий</span>
+                  <span className={styles.listValue}>{comment}</span>
+                </li>
+                <li className={styles.listItem}>
+                  <span className={styles.listKey}>Сумма</span>
+                  <span
+                    className={
+                      typeTx === 'income'
+                        ? `${styles.listValue} ${styles.income}`
+                        : `${styles.listValue} ${styles.outcome}`
+                    }
+                  >
+                    {sum.toFixed(2)}
+                  </span>
+                </li>
+                <li className={styles.listItem}>
+                  <span className={styles.listKey}>Баланс</span>
+                  <span className={styles.listValue}>{balance.toFixed(2)}</span>
+                </li>
+              </ul>
+            </div>
+          );
+        },
+      )}
+      {data.length === 0 && (
+        <div className={styles.plug}>
+          <p className={styles.plugText}>
+            Для того чтобы вести учёт необходимо ввести данные после нажатия
+            кнопки "<span className={styles.plugPlus}>+</span>"
+          </p>
         </div>
-      );
-    },
+      )}
+    </>
   );
 }
